64c14c3c9cf1a55007553638e648098e115685f2
codekaust <kaustubh1303@gmail.com>
2020-06-08 00:36:48 +0530

    update req.txt

--- django-server/django-rest-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kaustubh/Projects/MeshSOS/django-server/django-rest-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
